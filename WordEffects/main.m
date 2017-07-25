f1b7a77f4e915e07065765d6136777b32bf6dfc0
Tye Blackie <blackie.tye@gmail.com>
2017-07-25 13:56:04 -0700

    Initial commit

--- WordEffects/main.m
+++ WordEffects/main.m
@@ -8,10 +8,54 @@
 
 #import <Foundation/Foundation.h>
 
+
+
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+    
+    char playAgain = 'y';
+    
+    while (playAgain == 'y') {
+    
+        char inputChars [255];
+    
+        printf ("Input a string: ");
+        fgets (inputChars, 255, stdin);
+        strtok (inputChars, "\n");
+    
+        printf ("Your string is: %s", inputChars);
+        NSString *inputString = [NSString stringWithUTF8String:inputChars];
+    
+        NSLog(@"Here is your loud uppercase string : %@ \n",[inputString uppercaseString]);
+    
+        NSLog(@"Here is your quiet, lowercase string : %@ \n",[inputString lowercaseString]);
+    
+        NSLog(@"Here is your canadian string :  %@ \n", [inputString stringByAppendingString: @" Eh!"]);
+    
+        int value = [inputString intValue];
+          
+        NSLog(@"here is the int value of your string: %d \n", value);
+    
+        inputString = [inputString stringByReplacingOccurrencesOfString:@" " withString:@"-"];
+    
+        NSLog(@"Here is your string 'De-Spaced: %@ \n", inputString);
+    
+   
+    
+    if([inputString containsString:@"?"]) {
+           NSLog(@"I dont Know");
     }
+    
+    if([inputString containsString:@"!"]) {
+        NSLog(@"Whoa! Calm Down!");
+    }
+    }
+    
+
+    printf("Would you like to play again?");
+    fgets(&playAgain, 10, stdin);
+    
+    
     return 0;
 }
+
+
