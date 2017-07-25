c00da272d29bd72275f5486610fdc47c21c47a9e
Tye Blackie <blackie.tye@gmail.com>
2017-07-24 18:18:50 -0700

    Initial Commit

---
+++ WordEffects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WordEffects
+//
+//  Created by Tye Blackie on 2017-07-24.
+//  Copyright © 2017 Tye Blackie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
